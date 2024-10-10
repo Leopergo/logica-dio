7db5f1c919b6e88f3936b0e5767b7e0d3e9a8941
Leopergo <leonardogonc@gmail.com>
2024-10-10 20:40:23 -0300

    projeto finalizado

--- teste.js
+++ teste.js
@@ -1,36 +1,35 @@
 let nomeHeroi = prompt("Digite o nome do seu Herói: ");
+let xp = parseInt(prompt(`Em que nível o ${nomeHeroi} está?`));
+let tier;
 
-
-let xp = prompt(`Em que nível o ${nomeHeroi} está?`);
-
-while(xp < 1000 && xp < 10000){
-    let xp = prompt(`Em que nível o ${nomeHeroi} está?`);
+while(isNaN(xp)){
+    xp = parseInt(prompt(`Em que nível o ${nomeHeroi} está?`));
 }
 
-switch(xp){
-    case xp <= 1000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de Ferro`);
-        break;
-    case xp > 1000 && xp <= 2000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de Bronze`);
-        break;
-    case xp > 2000 && xp <= 5000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de Prata`);
-        break;
-    case xp > 5000 && xp <= 7000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de Ouro`);
-        break;
-    case xp > 7000 && xp <= 8000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de Platina`);
-        break;
-    case xp > 8000 && xp <= 9000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de Ascendente`);
-        break;
-    case xp > 9000 && xp <= 10000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de Imortal`);
-        break;
-    case xp > 8000 && xp <= 9000:
-        console.log(`O Herói de nome ${nomeHeroi}, está no nível de adiante`);
-        break;
+if(xp <= 1000) {
+    tier = "Ferro";
+}else if(xp > 1000 && xp <= 2000){
+    tier = "Bronze";
+}else if(xp > 2000 && xp <= 5000){
+    tier = "Prata";
+}else if(xp > 5000 && xp <= 7000){
+    tier = "Ouro";
+}else if(xp > 7000 && xp <= 8000){
+    tier = "Platina";      
+}else if(xp > 8000 && xp <= 9000){
+    tier = "Ascendente";     
+}else if(xp > 9000 && xp <= 10000){
+    tier = "Imortal";
+}else if(xp > 10000){
+    tier = "Radiante";   
+}else{
+    console.log("XP informado não é válido");
 }
+
+console.log(`O Herói de nome ${nomeHeroi}, está no nível de ${tier}`);
+
+    
+
+
+
 
